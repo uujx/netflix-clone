17e763dbeec8c20aa2750b8e0a84a780565806d0
uujx <esmhubx@gmail.com>
2020-08-08 23:43:10 +0800

    Update: debounce Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,33 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
+import _ from 'lodash'
+
 import axios from '../../axios-movies'
+import styles from './Search.module.scss'
 
 const Search: React.FC = () => {
-  const [movieName, setMovieName] = useState('')
+  const [search, setSearch] = useState('')
+  const lastestSearch = useRef<string>('')
+  const fetchResult = useCallback(
+    _.debounce(
+      () => {
+        console.log('fetching')
+        axios
+          .get(
+            `/search/movie?query=${encodeURIComponent(lastestSearch.current)}`
+          )
+          .then((res) => {
+            // TODO: route to the search result page
+            console.log(res)
+          })
+      },
+      500,
+      { leading: false, trailing: true }
+    ),
+    []
+  )
 
   const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMovieName(event.target.value)
-  }
+    setSearch(event.target.value)
+    lastestSearch.current = event.target.value
 
-  const onSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault()
-
-    axios
-      .get(`/search/movie?query=${encodeURIComponent(movieName)}`)
-      .then((res) => {
-        console.log(res)
-      })
+    if (event.target.value !== '') {
+      fetchResult()
+    }
+    // TODO: else route to the home page
   }
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input
-        type='text'
-        value={movieName}
-        onChange={inputChangeHandler}
-        placeholder='Search movies'
-      />
-      <button type='submit'>Search</button>
-    </form>
+    <input
+      className={styles.Input}
+      type='text'
+      value={search}
+      onChange={inputChangeHandler}
+      placeholder='Search movies'
+    />
   )
 }
 
