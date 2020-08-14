d549faaf7a6d29a5d37c29eb6a7aae29000fa3da
uujx <esmhubx@gmail.com>
2020-08-15 03:08:05 +0800

    Update: clickable netflix logo

--- src/components/UI/NetflixLogo/NetflixLogo.tsx
+++ src/components/UI/NetflixLogo/NetflixLogo.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import netflixLogo from '../../../assets/images/Netflix_Logo_RGB.png'
 import styles from './NetflixLogo.module.scss'
 
-
 const NetflixLogo: React.FC = (props) => {
+  const history = useHistory()
+
+  const onClickHandler = () => {
+    history.push('/')
+  }
+
   return (
-    <div className={styles.LogoContainer}>
+    <div className={styles.LogoContainer} onClick={onClickHandler}>
       <img src={netflixLogo} alt='Netflix' className={styles.Logo} />
     </div>
   )
