fbf7abba0b256580dd1a8f2f97492ac37b3085fd
uujx <esmhubx@gmail.com>
2020-08-20 04:23:54 +0800

    Add: basic server

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,38 +1,91 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const dotenv = require('dotenv')
+const webpack = require('webpack')
+
+const PostCSSLoader = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: [require('autoprefixer')]
+  }
+}
+
+const env = dotenv.config().parsed
+const processEnv = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next])
+  return prev
+}, {})
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
 
   mode: 'production',
 
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
 
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss']
   },
 
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.ts(x?)$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.module\.(sc|sa|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[local]--[hash:base64:6]',
+                exportLocalsConvention: 'camelCase'
+              }
+            }
+          },
+          PostCSSLoader,
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(sc|sa|c)ss$/,
+        exclude: /\.module\.(sc|sa|c)ss$/,
+        use: ['style-loader', 'css-loader', PostCSSLoader, 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: 'url-loader'
+        }
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svg-react-loader'
+        }
       }
     ]
   },
 
   plugins: [
     new CleanWebpackPlugin(),
+    new webpack.DefinePlugin(processEnv),
     new HtmlWebpackPlugin({
       title: 'Netflix Clone',
-      template: './dist/index.html'
+      template: './src/index.html'
     })
   ]
 }
